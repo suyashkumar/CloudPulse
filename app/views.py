e4a5245b4e9f89a32d92b9eac6861ab58a494885
Suyash Kumar <suyash.kumar@duke.edu>
2015-08-11 00:19:28 -0400

    Slight reorg and doc of views.py

--- app/views.py
+++ app/views.py
@@ -13,36 +13,46 @@ import datetime
 from bson.objectid import ObjectId
 
 
-collection=connection['test4'].users
+collection=connection['test4'].users 
 
 
 def event_stream(): 
-    messages=SSEClient('https://api.particle.io/v1/devices/events?access_token=6a61e063f79781dddcc39729e77ed76696f23bfc')
-    print type(messages) 
-    msgGen=process.parse(messages.__iter__())  
-    for item in msgGen:
+    messages=SSEClient('https://api.particle.io/v1/devices/events?access_token=6a61e063f79781dddcc39729e77ed76696f23bfc') # Get iterator over SSE messages from the device 
+    msgGen=process.parse(messages.__iter__()) # Get generator over FULL assembled packages of data from device
+    for item in msgGen: # For each full package of data from the device: 
         print item
-        # Save to database
+        # Save this exam data to database
         exam=collection.Exam()
         exam['name']=unicode(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         exam['data']=item
         exam['datestr']=u''
         exam.save()
         print exam['_id']
-        yield "data: %s\n\n" % str(exam['_id'])
-        #yield "data: {\"data\": %s}\n\n" % str(item)
+        
+        yield "data: %s\n\n" % str(exam['_id']) # Yield generator with the unique ID of this exam to the client
+        
 @app.route('/data_source')
 def sse_request():
+    """
+    Pushes new messages containing database unique ids of new exams to open sockets.  
+    """
     return Response(
             event_stream(),
             mimetype='text/event-stream')
 
 @app.route('/')
 def page():
+    """
+    Send index over at route '/' 
+    """
     print "main"
     return app.send_static_file('index.html') 
+
 @app.route('/api/list', methods=['GET'])
 def list():
+    """
+    Respond with a list of all exams. Currently also sends exam data, id, and name.
+    """
     returnDict={'name':[],'data':[],'id':[]}
     print "hello"
     collection=connection['test4'].users
@@ -56,24 +66,28 @@ def list():
         returnDict['id'].append(item._id.__str__())
 
     return Response(json.dumps(returnDict), mimetype='application/json')
+
 @app.route('/api/del/<id>')
 def delete(id):
+    """
+    Deletes the exam with id <id> from the database.
+    """ 
     collection.remove({"_id":ObjectId(id)})
     print "del"
     return "ok"
 
 @app.route('/api/download/<id>')
 def download(id):
+    """
+    Generates CSV of the exam with id <id> and returns it to client as a file for download. 
+    """
+    # First make csv string:
     item=collection.find({'_id':ObjectId(id)})
     retStr=str(item[0]['data']) 
-    csv=retStr[1:len(retStr)-1]
-    print csv
-    # We need to modify the response, so the first thing we 
-    # need to do is create a response out of the CSV string
-    response = make_response(csv)
-    # This is the key: Set the right header for the response
-    # to be downloaded, instead of just printed on the browser
-    response.headers["Content-Disposition"] = "attachment; filename=pulsedata.csv"
+    csv=retStr[1:len(retStr)-1] 
+
+    response = make_response(csv) # Add csv string to response 
+    response.headers["Content-Disposition"] = "attachment; filename=pulsedata.csv" # Set right headers for response
     return response
 
 
